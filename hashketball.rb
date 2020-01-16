feb80d05d501ff199406328c56718f1e4a580e49
Jack Parker <jprkr96@gmail.com>
2020-01-16 15:20:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,6 +1,10 @@
 # Write your code here!
 def game_hash 
-  gamehash = {:home =>
+  gamehash = {:home => {:team_name => "",
+    :colors => [''],
+    :players => [{},{}]
+  },
+  :away => {}}
 end
 
 
