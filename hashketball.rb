fa8df9188b31dbd74182023c6b5b3c16acd2cfd0
Jack Parker <jprkr96@gmail.com>
2020-01-16 15:37:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,17 @@
 def game_hash 
   gamehash = {:home => {:team_name => "Brooklyn Nets",
     :colors => ['Black','White'],
-    :players => [{:player_name => },{:number}]
+    :players => [{:player_name => {
+    {number: },
+    {shoe: },
+    {points: },
+    {rebounds: },
+    {assists: },
+    {steals: },
+    {blocks: },
+    {slamm_dunks: } },
+    {:player_name2 => }
+    ]
   },
   :away => {:team_name => "",
     :colors => [''],
