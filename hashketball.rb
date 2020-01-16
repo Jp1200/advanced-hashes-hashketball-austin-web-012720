c331fff30c9aa2212d4e8838a0376e264a5526fb
Jack Parker <jprkr96@gmail.com>
2020-01-16 18:30:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -209,7 +209,7 @@ def big_shoe_rebounds
   #  end
  # i += 1 
   #end
-  if (gamehash[:home][:players].max_by{|i| i[:shoe]} > gamehash[:away][:players].max_by{|i| i[:shoe]})
+  if (gamehash[:home][:players].max_by{|i| i[:shoe]} >     gamehash[:away][:players].max_by{|i| i[:shoe]})
     
      bigshoename = gamehash[:home][:players].max_by{|i| i[:shoe]}[:player_name]
   else 
