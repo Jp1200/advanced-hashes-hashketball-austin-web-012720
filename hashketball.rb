b789af855535c0fa6b5c976e43e533fd82b5eb40
Jack Parker <jprkr96@gmail.com>
2020-01-16 16:44:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -115,8 +115,6 @@ def num_points_scored(player_name)
       score = gamehash[:home][:players][player_index][:points]
       elsif player_name == gamehash[:away][:players][player_index][:player_name]
       score = gamehash[:away][:players][player_index][:points]
-      
-      
       end
       player_index += 1 
     end
